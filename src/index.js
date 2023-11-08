110bb0c376ad4372241e039dfd088ce1de5cdf44
AkhmalNovanda <cocwar427@gmail.com>
2023-11-08 10:16:32 +0700

    destructuring folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dasboard from "./pages/Dasboard";
 import Navbar from "./components/Navbar";
@@ -22,8 +20,3 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
