ead1fd3e9dad5849d4c58ee68e2792fdbc8e4268
intchl <126045100+intchl@users.noreply.github.com>
2023-11-09 10:00:54 +0700

    new file dashboard admin

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dasboard from "./pages/users/Dasboard";  
+import Dasboard from "./pages/users/Dasboard";
 import IzinKerja from "./pages/users/IzinKerja";
 import MasukKerja from "./pages/users/MasukKerja";
 import Telat from "./pages/users/Telat";
+import DB_Admin from "./pages/admin/DB_Admin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,6 +15,7 @@ root.render(
       <Route path="/masuk_kerja" Component={MasukKerja} />
       <Route path="/izin_kerja" Component={IzinKerja} />
       <Route path="telat_kerja" Component={Telat} />
+      <Route path="/db_admin" Component={DB_Admin} />
     </Routes>
   </BrowserRouter>
 );
