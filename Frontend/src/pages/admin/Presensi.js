84dbdd4bdfc60d685b5787f8e74997bd93d741b8
intchl <126045100+intchl@users.noreply.github.com>
2023-11-10 09:04:34 +0700

    bg color sidebar

--- Frontend/src/pages/admin/Presensi.js
+++ Frontend/src/pages/admin/Presensi.js
@@ -15,7 +15,7 @@ function Presensi() {
   return (
     <div id="wrapper">
       <Sidebar />
-      <div className="container-fluid p-5 w-100">
+      <div className="admin-bg container-fluid p-5 w-100">
         <div className="card shadow mb-4">
           {/* Card Header - Dropdown */}
           <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -48,24 +48,20 @@ function Presensi() {
               rowsPerPageOptions={[5, 10, 25, 50]}
               tableStyle={{ minWidth: "50rem" }}
               className="customDataTable" // Add a custom class for more styling options
-              paginatorTemplate={`CurrentPageReport PrevPageLink PageLinks NextPageLink `}
-            >
+              paginatorTemplate={`CurrentPageReport PrevPageLink PageLinks NextPageLink `}>
               <Column field="ID" header="ID" style={{ width: "10%" }}></Column>
               <Column
                 field="Nama"
                 header="Nama"
-                style={{ width: "25%" }}
-              ></Column>
+                style={{ width: "25%" }}></Column>
               <Column
                 field="Divisi"
                 header="Divisi"
-                style={{ width: "25%" }}
-              ></Column>
+                style={{ width: "25%" }}></Column>
               <Column
                 field="Asal Kampus"
                 header="Asal Kampus"
-                style={{ width: "30%" }}
-              ></Column>
+                style={{ width: "30%" }}></Column>
               <Column field="Status" header="status" style={{ width: "10%" }}>
                 <div>
                   <Button variant="dark" />
