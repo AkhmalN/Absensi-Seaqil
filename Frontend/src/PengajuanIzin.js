68fdb445e6688460bae0d38f18f4ce931558e12c
nailufarfarha <nailufarfaah@gmail.com>
2023-11-14 09:09:17 +0700

    create table pengajuan

---
+++ Frontend/src/PengajuanIzin.js
@@ -0,0 +1,46 @@
+const PengajuanIzin = [
+  {
+    No: 1,
+    Tanggal: "11/11/2023",
+    Pengajuan: "Sakit",
+    Status: "Diterima",
+  },
+  {
+    No: 2,
+    Tanggal: "11/11/2023",
+    Pengajuan: "Sakit",
+    Status: "Diterima",
+  },
+  {
+    No: 3,
+    Tanggal: "11/11/2023",
+    Pengajuan: "Sakit",
+    Status: "Diterima",
+  },
+  {
+    No: 4,
+    Tanggal: "11/11/2023",
+    Pengajuan: "Sakit",
+    Status: "Diterima",
+  },
+  {
+    No: 5,
+    Tanggal: "11/11/2023",
+    Pengajuan: "Sakit",
+    Status: "Diterima",
+  },
+  {
+    No: 6,
+    Tanggal: "11/11/2023",
+    Pengajuan: "Sakit",
+    Status: "Diterima",
+  },
+  {
+    No: 7,
+    Tanggal: "11/11/2023",
+    Pengajuan: "Sakit",
+    Status: "Diterima",
+  },
+];
+
+export default PengajuanIzin;
--- Frontend/src/pages/admin/Dashboard.js
+++ Frontend/src/pages/admin/Dashboard.js
@@ -26,22 +26,13 @@ const Dashboard = () => {
             {/* Topbar */}
             <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
               {/* Sidebar Toggle (Topbar) */}
-              <button
-                id="sidebarToggleTop"
-                className="btn btn-link d-md-none rounded-circle mr-3"
-              >
+              <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
                 <i className="fa fa-bars" />
               </button>
               {/* Topbar Search */}
               <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                 <div className="input-group">
-                  <input
-                    type="text"
-                    className="form-control border border-secondary small"
-                    placeholder="Search for..."
-                    aria-label="Search"
-                    aria-describedby="basic-addon2"
-                  />
+                  <input type="text" className="form-control border border-secondary small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
                   <div className="input-group-append">
                     <button className="btn btn-primary" type="button">
                       <i className="fas fa-search fa-sm" />
@@ -56,13 +47,8 @@ const Dashboard = () => {
                 <div className="nav-item dropdown no-arrow">
                   <Dropdown show={isDropdownOpen} onToggle={toggleDropdown}>
                     <Dropdown.Toggle variant="white">
-                      <span className="mr-2 d-none d-lg-inline text-gray-600 small">
-                        Testing Doang
-                      </span>
-                      <img
-                        className="img-profile rounded-circle"
-                        src="img/undraw_profile.svg"
-                      />
+                      <span className="mr-2 d-none d-lg-inline text-gray-600 small">Testing Doang</span>
+                      <img className="img-profile rounded-circle" src="img/undraw_profile.svg" />
                     </Dropdown.Toggle>
                     {/* Dropdown - User Information */}
                     <Dropdown.Menu className="dropdown-menu-right shadow animated--grow-in">
@@ -79,11 +65,7 @@ const Dashboard = () => {
                         Activity Log
                       </Dropdown.Item>
                       <Dropdown.Divider />
-                      <Dropdown.Item
-                        href="#"
-                        data-toggle="modal"
-                        data-target="#logoutModal"
-                      >
+                      <Dropdown.Item href="#" data-toggle="modal" data-target="#logoutModal">
                         <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
                         Logout
                       </Dropdown.Item>
@@ -98,12 +80,8 @@ const Dashboard = () => {
               {/* Page Heading */}
               <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-                <a
-                  href="#"
-                  className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
-                >
-                  <i className="fas fa-download fa-sm text-white-50" /> Generate
-                  Report
+                <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                  <i className="fas fa-download fa-sm text-white-50" /> Generate Report
                 </a>
               </div>
               {/* Content Row */}
@@ -114,12 +92,8 @@ const Dashboard = () => {
                     <div className="card-body">
                       <div className="row no-gutters align-items-center">
                         <div className="col mr-2">
-                          <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Earnings (Monthly)
-                          </div>
-                          <div className="h5 mb-0 font-weight-bold text-gray-800">
-                            $40,000
-                          </div>
+                          <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
+                          <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
                         </div>
                         <div className="col-auto">
                           <i className="fas fa-calendar fa-2x text-gray-300" />
@@ -134,12 +108,8 @@ const Dashboard = () => {
                     <div className="card-body">
                       <div className="row no-gutters align-items-center">
                         <div className="col mr-2">
-                          <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
-                            Earnings (Annual)
-                          </div>
-                          <div className="h5 mb-0 font-weight-bold text-gray-800">
-                            $215,000
-                          </div>
+                          <div className="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
+                          <div className="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
                         </div>
                         <div className="col-auto">
                           <i className="fas fa-dollar-sign fa-2x text-gray-300" />
@@ -154,25 +124,14 @@ const Dashboard = () => {
                     <div className="card-body">
                       <div className="row no-gutters align-items-center">
                         <div className="col mr-2">
-                          <div className="text-xs font-weight-bold text-info text-uppercase mb-1">
-                            Tasks
-                          </div>
+                          <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
                           <div className="row no-gutters align-items-center">
                             <div className="col-auto">
-                              <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                                50%
-                              </div>
+                              <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
                             </div>
                             <div className="col">
                               <div className="progress progress-sm mr-2">
-                                <div
-                                  className="progress-bar bg-info"
-                                  role="progressbar"
-                                  style={{ width: "50%" }}
-                                  aria-valuenow={50}
-                                  aria-valuemin={0}
-                                  aria-valuemax={100}
-                                />
+                                <div className="progress-bar bg-info" role="progressbar" style={{ width: "50%" }} aria-valuenow={50} aria-valuemin={0} aria-valuemax={100} />
                               </div>
                             </div>
                           </div>
@@ -190,12 +149,8 @@ const Dashboard = () => {
                     <div className="card-body">
                       <div className="row no-gutters align-items-center">
                         <div className="col mr-2">
-                          <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                            Pending Requests
-                          </div>
-                          <div className="h5 mb-0 font-weight-bold text-gray-800">
-                            18
-                          </div>
+                          <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+                          <div className="h5 mb-0 font-weight-bold text-gray-800">18</div>
                         </div>
                         <div className="col-auto">
                           <i className="fas fa-comments fa-2x text-gray-300" />
@@ -211,9 +166,7 @@ const Dashboard = () => {
                 <div className="card shadow mb-4">
                   {/* Card Header - Dropdown */}
                   <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 className="m-0 font-weight-bold text-primary">
-                      Data Mahasiswa
-                    </h6>
+                    <h6 className="m-0 font-weight-bold text-primary">Data Mahasiswa</h6>
                   </div>
                   {/* Card Body */}
                   <div className="card-body">
@@ -226,26 +179,10 @@ const Dashboard = () => {
                       className="customDataTable" // Add a custom class for more styling options
                       paginatorTemplate={`CurrentPageReport PrevPageLink PageLinks NextPageLink `}
                     >
-                      <Column
-                        field="ID"
-                        header="ID"
-                        style={{ width: "10%" }}
-                      ></Column>
-                      <Column
-                        field="Nama"
-                        header="Nama"
-                        style={{ width: "25%" }}
-                      ></Column>
-                      <Column
-                        field="Divisi"
-                        header="Divisi"
-                        style={{ width: "30%" }}
-                      ></Column>
-                      <Column
-                        field="Asal Kampus"
-                        header="Asal Kampus"
-                        style={{ width: "35%" }}
-                      ></Column>
+                      <Column field="ID" header="ID" style={{ width: "10%" }}></Column>
+                      <Column field="Nama" header="Nama" style={{ width: "25%" }}></Column>
+                      <Column field="Divisi" header="Divisi" style={{ width: "30%" }}></Column>
+                      <Column field="Asal Kampus" header="Asal Kampus" style={{ width: "35%" }}></Column>
                     </DataTable>
                   </div>
                 </div>
