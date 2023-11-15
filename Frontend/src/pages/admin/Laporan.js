2ea305ca3616cf0bd856e1120afd01ee28857674
sephiamulya <mulyawati.sephia@gmail.com>
2023-11-15 15:55:11 +0700

    profile admin

--- Frontend/src/pages/admin/Laporan.js
+++ Frontend/src/pages/admin/Laporan.js
@@ -1,19 +1,36 @@
 import React from "react";
+/* global DatePicker */
+import datepicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import Sidebar from "../../components/Sidebar";
-import "../../utils/css/sb-admin-2.min.css";
 import { useState } from "react";
-import { Dropdown } from "react-bootstrap";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import DATA from "../../DATA";
 import "../../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-const Laporan = () => {
+import {
+  faBars,
+  faCaretDown,
+  faCogs,
+  faList,
+  faMagnifyingGlass,
+  faSignOut,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { Dropdown } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import profil from "../../assets/admin/profil.png";
+function Laporan() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
 
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleChange = (date) => {
+    setSelectedDate(date);
   };
 
   return (
@@ -33,7 +50,7 @@ const Laporan = () => {
                 id="sidebarToggleTop"
                 className="btn btn-link d-md-none rounded-circle mr-3"
               >
-                <i className="fa fa-bars" />
+                <FontAwesomeIcon icon={faBars} />
               </button>
               {/* Topbar Navbar */}
               <ul className="navbar-nav ml-auto">
@@ -42,27 +59,47 @@ const Laporan = () => {
                 <div className="nav-item dropdown no-arrow">
                   <Dropdown show={isDropdownOpen} onToggle={toggleDropdown}>
                     <Dropdown.Toggle variant="white">
-                      <span className="mr-2 d-none d-lg-inline text-black">
-                        IC
-                      </span>
-                      <img
-                        alt=""
-                        className="img-profile rounded-circle"
-                        src="../../admin/profil.png"
-                      />
+                      <Row className="d-flex align-content-end">
+                        <Col xs="3" className="">
+                          <img
+                            src={profil}
+                            alt="logo"
+                            className="icon_profile"
+                          />
+                        </Col>
+                        <Col xs="7" className="mt-1">
+                          <Row>
+                            <span className="header_profile d-none d-lg-inline text-left ">
+                              Asep Sutrisno
+                            </span>
+                          </Row>
+                          <Row>
+                            <span className="sub-header_profile d-none d-lg-inline text-left ">
+                              Koordinator
+                            </span>
+                          </Row>
+                        </Col>
+                        <Col xs="1" className="mt-2 ps-3">
+                          <FontAwesomeIcon
+                            icon={faCaretDown}
+                            className="sub-header_profile d-none d-lg-inline text-left "
+                          />
+                        </Col>
+                      </Row>
                     </Dropdown.Toggle>
                     {/* Dropdown - User Information */}
                     <Dropdown.Menu className="dropdown-menu-right shadow animated--grow-in">
                       <Dropdown.Item href="#">
+                        <FontAwesomeIcon icon={faUser} />
                         <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400" />
                         Profile
                       </Dropdown.Item>
                       <Dropdown.Item href="#">
-                        <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400" />
+                        <FontAwesomeIcon icon={faCogs} />
                         Settings
                       </Dropdown.Item>
                       <Dropdown.Item href="#">
-                        <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400" />
+                        <FontAwesomeIcon icon={faList} />
                         Activity Log
                       </Dropdown.Item>
                       <Dropdown.Divider />
@@ -71,7 +108,7 @@ const Laporan = () => {
                         data-toggle="modal"
                         data-target="#logoutModal"
                       >
-                        <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
+                        <FontAwesomeIcon icon={faSignOut} />
                         Logout
                       </Dropdown.Item>
                     </Dropdown.Menu>
@@ -81,40 +118,35 @@ const Laporan = () => {
             </nav>
             {/* End of Topbar */}
             {/* Begin Page Content */}
-            <div className="container-fluid" id="beranda">
-              {/* Area Chart PRESENSI PULANG HARI INI*/}
+            <div className="container-fluid admin-bg w-100" id="beranda">
               <div className="card shadow mb-4">
+                {/* Card Header - Dropdown */}
                 <div className="card-header py-3 d-flex justify-content-between bg-white">
                   <div className="header tulisan">
                     <div className="header">
                       Laporan
+                      {/* <span className="sub-header ms-2">27/10/2023</span> */}
                       <span className="blue-tag ms-2">MSIB Batch 5</span>
                     </div>
                     <div className="sub-header">SEAMEO QITEP In Language</div>
                   </div>
-                  <div className="header-buttons">
-                    <form className="d-sm-inline-block form-inline mr-0 mw-100 navbar-search">
-                      <div className="input-group">
-                        <input
-                          type="text"
-                          className="form-control border border-secondary small"
-                          placeholder="Cari..."
-                          aria-label="Search"
-                          aria-describedby="basic-addon2"
-                        />
-                        <div className="input-group-append">
-                          <button
-                            className="btn btn-primary search-button"
-                            type="button"
-                          >
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
-                          </button>
-                        </div>
-                      </div>
-                    </form>
+                  {/* <div className="date-picker-container">
+                    <DatePicker
+                      selected={selectedDate}
+                      onChange={handleChange}
+                      dateFormat="dd/MM/yyyy"
+                      className="date-picker"
+                    />
+                    <FontAwesomeIcon
+                      icon={faCaretDown}
+                      className="dropdown-icon"
+                    /> */}
+                  {/* </div> */}
+                  <form className="d-flex align-items-center form-inline mr-0 mw-100 navbar-search">
                     <button
-                      className="btn btn-secondary custom-print-button"
+                      className="print-button"
                       type="button"
+                      style={{ marginRight: "16px" }}
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -131,7 +163,21 @@ const Laporan = () => {
                       </svg>
                       Cetak
                     </button>
-                  </div>
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        className="search-button"
+                        placeholder="Cari"
+                        aria-label="Search"
+                        aria-describedby="basic-addon2"
+                      />
+                      <div className="input-group-append">
+                        <button className="btn btn-primary" type="button">
+                          <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
 
                 {/* Card Body */}
@@ -142,7 +188,7 @@ const Laporan = () => {
                     rows={6}
                     rowsPerPageOptions={[5, 10, 25, 50]}
                     tableStyle={{ textAlign: "center" }}
-                    className="customDataTable" // Add a custom class for more styling options
+                    className="customDataTable"
                     paginatorTemplate={`CurrentPageReport PrevPageLink PageLinks NextPageLink `}
                   >
                     <Column
@@ -187,14 +233,21 @@ const Laporan = () => {
                               paddingTop: "5px", // Adjust the padding on top
                             }}
                           >
-                            <div style={{ width: "17%" }}>Hadir</div>
-                            <div style={{ width: "16%" }}>Izin</div>
-                            <div style={{ width: "16%" }}>Sakit</div>
+                            <div style={{ width: "17%", margin: "0 10px" }}>
+                              Hadir
+                            </div>
+                            <div style={{ width: "16%", margin: "0 10px" }}>
+                              Izin
+                            </div>
+                            <div style={{ width: "16%", margin: "0 10px" }}>
+                              Sakit
+                            </div>
                             <div
                               style={{
                                 width: "47%",
                                 paddingRight: "5px",
                                 whiteSpace: "nowrap",
+                                margin: "0 10px",
                               }}
                             >
                               Tanpa Keterangan
@@ -202,14 +255,13 @@ const Laporan = () => {
                           </div>
                         </>
                       }
-                      style={{ width: "35%" }} // Adjust the width of the "Status" column
+                      style={{ width: "20%" }} // Adjust the width of the "Status" column
                       alignHeader={"center"}
                       rowSpan={2}
                     ></Column>
                   </DataTable>
                 </div>
               </div>
-              {/* Content Row */}
             </div>
             {/* /.container-fluid */}
           </div>
@@ -229,6 +281,6 @@ const Laporan = () => {
       {/* End of Page Wrapper */}
     </>
   );
-};
+}
 
 export default Laporan;
