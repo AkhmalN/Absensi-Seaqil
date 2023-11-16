7bd4de3a09a2e916b5dea12b51f63e5eb767b579
nailufarfarha <nailufarfaah@gmail.com>
2023-11-16 11:40:37 +0700

    create rekap presensi

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,11 +5,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IzinKerja from "./pages/users/IzinKerja";
 import MasukKerja from "./pages/users/MasukKerja";
 import StatusPengajuan from "./pages/users/StatusPengajuan";
+import RekapPresensi from "./pages/users/RekapPresensi";
 import Dashboard from "./pages/admin/Dashboard";
 import Pengajuan from "./pages/admin/Pengajuan";
 import DataMahasiswa from "./pages/admin/DataMahasiswa";
 import DataIzin from "./pages/admin/DataIzin";
 import formmasukkerja from "./components/FormMasukKerja";
+import Laporan from "./pages/admin/Laporan";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,6 +20,13 @@ root.render(
       <Route path="/masuk_kerja" Component={MasukKerja} />
       <Route path="/izin_kerja" Component={IzinKerja} />
       <Route path="/status_pengajuan" Component={StatusPengajuan} />
+      <Route path="/rekap_presensi" Component={RekapPresensi} />
+      <Route path="/dashboard" Component={Dashboard} />
+      <Route path="/dashboard/DataIzin" Component={DataIzin} />
+      <Route path="/dashboard/pengajuan" Component={Pengajuan} />
+      <Route path="/dashboard/data_mahasiswa" Component={DataMahasiswa} />
+      <Route path="/dashboard/form_masuk_kerja" Component={formmasukkerja} />
+      <Route path="/dashboard/Laporan" Component={Laporan} />
     </Routes>
   </BrowserRouter>
 );
