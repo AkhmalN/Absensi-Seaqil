d9e81d66877154457150c238d5ee317d49c6e3a5
nailufarfarha <nailufarfaah@gmail.com>
2023-11-16 08:33:01 +0700

    add bg blur

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dasboard from "./pages/users/Dasboard";
 import IzinKerja from "./pages/users/IzinKerja";
 import MasukKerja from "./pages/users/MasukKerja";
-import Telat from "./pages/users/Telat";
 import StatusPengajuan from "./pages/users/StatusPengajuan";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,7 +14,6 @@ root.render(
       <Route path="/" Component={Dasboard} />
       <Route path="/masuk_kerja" Component={MasukKerja} />
       <Route path="/izin_kerja" Component={IzinKerja} />
-      <Route path="telat_kerja" Component={Telat} />
       <Route path="/status_pengajuan" Component={StatusPengajuan} />
     </Routes>
   </BrowserRouter>
