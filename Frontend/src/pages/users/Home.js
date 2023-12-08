d158a0b0e4a506ddbb012ea15a45090614a24711
NaufalHalimGibran <naufalgibran11.nhg@gmail.com>
2023-12-08 15:07:15 +0700

    Home

--- Frontend/src/pages/users/Home.js
+++ Frontend/src/pages/users/Home.js
@@ -538,7 +538,7 @@ const Home = () => {
                             <div className="col">
                               <div class="mb-3">
                                 <label for="pres_pulang" class="form-label">
-                                  Presensi Pulang
+                                  Lokasi
                                 </label>
                                 <input
                                   type="text"
